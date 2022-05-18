a6d463bae72de84aa3d82d03757d5cb2a1e3c173
ProgressGuard <help@progressguard.com>
2022-05-18 09:26:28 +0300

    Add BottomSheet code.

---
+++ Sources/EssentialUI/BottomSheet/BottomSheetModifier.swift
@@ -0,0 +1,188 @@
+//
+//  BottomSheetModifier.swift
+//  EssentialUI
+//
+//  Created by Gene Bogdanovich on 18.05.22.
+//
+
+import SwiftUI
+
+struct BottomSheet<T: Any, ContentView: View>: ViewModifier {
+    @Binding private var isPresented: Bool
+    
+    private let detents: [UISheetPresentationController.Detent]
+    private let largestUndimmedDetentIdentifier: UISheetPresentationController.Detent.Identifier?
+    private let prefersGrabberVisible: Bool
+    private let prefersScrollingExpandsWhenScrolledToEdge: Bool
+    private let prefersEdgeAttachedInCompactHeight: Bool
+    @Binding private var selectedDetentIdentifier: UISheetPresentationController.Detent.Identifier?
+    private let widthFollowsPreferredContentSizeWhenEdgeAttached: Bool
+    private let isModalInPresentation: Bool
+    private var onDismiss: (() -> Void)?
+    private let contentView: () -> ContentView
+    
+    @State private var bottomSheetViewController: BottomSheetViewController<ContentView>?
+    
+    init(
+        isPresented: Binding<Bool>,
+        detents: [UISheetPresentationController.Detent] = [.medium(), .large()],
+        largestUndimmedDetentIdentifier: UISheetPresentationController.Detent.Identifier? = nil,
+        prefersGrabberVisible: Bool = false,
+        prefersScrollingExpandsWhenScrolledToEdge: Bool = true,
+        prefersEdgeAttachedInCompactHeight: Bool = false,
+        selectedDetentIdentifier: Binding<UISheetPresentationController.Detent.Identifier?> = Binding.constant(nil),
+        widthFollowsPreferredContentSizeWhenEdgeAttached: Bool = false,
+        isModalInPresentation: Bool = false,
+        onDismiss: (() -> Void)? = nil,
+        @ViewBuilder contentView: @escaping () -> ContentView
+    ) {
+        _isPresented = isPresented
+        
+        self.detents = detents
+        self.largestUndimmedDetentIdentifier = largestUndimmedDetentIdentifier
+        self.prefersGrabberVisible = prefersGrabberVisible
+        self.prefersScrollingExpandsWhenScrolledToEdge = prefersScrollingExpandsWhenScrolledToEdge
+        self.prefersEdgeAttachedInCompactHeight = prefersEdgeAttachedInCompactHeight
+        self._selectedDetentIdentifier = selectedDetentIdentifier
+        self.widthFollowsPreferredContentSizeWhenEdgeAttached = widthFollowsPreferredContentSizeWhenEdgeAttached
+        self.isModalInPresentation = isModalInPresentation
+        self.contentView = contentView
+        self.onDismiss = onDismiss
+    }
+    
+    init(
+        item: Binding<T?>,
+        detents: [UISheetPresentationController.Detent] = [.medium(), .large()],
+        largestUndimmedDetentIdentifier: UISheetPresentationController.Detent.Identifier? = nil,
+        prefersGrabberVisible: Bool = false,
+        prefersScrollingExpandsWhenScrolledToEdge: Bool = true,
+        prefersEdgeAttachedInCompactHeight: Bool = false,
+        selectedDetentIdentifier: Binding<UISheetPresentationController.Detent.Identifier?> = Binding.constant(nil),
+        widthFollowsPreferredContentSizeWhenEdgeAttached: Bool = false,
+        isModalInPresentation: Bool = false,
+        onDismiss: (() -> Void)? = nil,
+        @ViewBuilder contentView: @escaping () -> ContentView
+    ) {
+        self._isPresented = Binding<Bool>(get: {
+            item.wrappedValue != nil
+        }, set: { newValue in
+            item.wrappedValue = nil
+        })
+        self.detents = detents
+        self.largestUndimmedDetentIdentifier = largestUndimmedDetentIdentifier
+        self.prefersGrabberVisible = prefersGrabberVisible
+        self.prefersScrollingExpandsWhenScrolledToEdge = prefersScrollingExpandsWhenScrolledToEdge
+        self.prefersEdgeAttachedInCompactHeight = prefersEdgeAttachedInCompactHeight
+        self._selectedDetentIdentifier = selectedDetentIdentifier
+        self.widthFollowsPreferredContentSizeWhenEdgeAttached = widthFollowsPreferredContentSizeWhenEdgeAttached
+        self.isModalInPresentation = isModalInPresentation
+        self.contentView = contentView
+        self.onDismiss = onDismiss
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .onChange(of: isPresented, perform: updatePresentation)
+            .onChange(of: selectedDetentIdentifier, perform: updateSelectedDetentIdentifier)
+    }
+    
+    private func updatePresentation(_ isPresented: Bool) {
+        guard let windowScene = UIApplication.shared.connectedScenes.first(where: {
+            $0.activationState == .foregroundActive
+        }) as? UIWindowScene else { return }
+        
+        guard let root = windowScene.keyWindow?.rootViewController else { return }
+        var controllerToPresentFrom = root
+        while let presented = controllerToPresentFrom.presentedViewController {
+            controllerToPresentFrom = presented
+        }
+        
+        if isPresented {
+            bottomSheetViewController = BottomSheetViewController(
+                isPresented: $isPresented,
+                detents: detents,
+                largestUndimmedDetentIdentifier: largestUndimmedDetentIdentifier,
+                prefersGrabberVisible: prefersGrabberVisible,
+                prefersScrollingExpandsWhenScrolledToEdge: prefersScrollingExpandsWhenScrolledToEdge,
+                prefersEdgeAttachedInCompactHeight: prefersEdgeAttachedInCompactHeight,
+                selectedDetentIdentifier: $selectedDetentIdentifier,
+                widthFollowsPreferredContentSizeWhenEdgeAttached: widthFollowsPreferredContentSizeWhenEdgeAttached,
+                isModalInPresentation: isModalInPresentation,
+                content: contentView()
+            )
+            
+            if let controller = bottomSheetViewController {
+                controllerToPresentFrom.present(controller, animated: true)
+            }
+        } else {
+            onDismiss?()
+            bottomSheetViewController?.dismiss(animated: true)
+        }
+    }
+    
+    private func updateSelectedDetentIdentifier(_ selectedDetentIdentifier: UISheetPresentationController.Detent.Identifier?) {
+        bottomSheetViewController?.updateSelectedDetentIdentifier(selectedDetentIdentifier)
+    }
+}
+
+extension View {
+    public func bottomSheet<ContentView: View>(
+        isPresented: Binding<Bool>,
+        detents: [UISheetPresentationController.Detent] = [.medium(), .large()],
+        largestUndimmedDetentIdentifier: UISheetPresentationController.Detent.Identifier? = nil,
+        prefersGrabberVisible: Bool = false,
+        prefersScrollingExpandsWhenScrolledToEdge: Bool = true,
+        prefersEdgeAttachedInCompactHeight: Bool = false,
+        selectedDetentIdentifier: Binding<UISheetPresentationController.Detent.Identifier?> = Binding.constant(nil),
+        widthFollowsPreferredContentSizeWhenEdgeAttached: Bool = false,
+        isModalInPresentation: Bool = false,
+        onDismiss: (() -> Void)? = nil,
+        @ViewBuilder contentView: @escaping () -> ContentView
+    ) -> some View {
+        self.modifier(
+            BottomSheet<Any, ContentView>(
+                isPresented: isPresented,
+                detents: detents,
+                largestUndimmedDetentIdentifier: largestUndimmedDetentIdentifier,
+                prefersGrabberVisible: prefersGrabberVisible,
+                prefersScrollingExpandsWhenScrolledToEdge: prefersScrollingExpandsWhenScrolledToEdge,
+                prefersEdgeAttachedInCompactHeight: prefersEdgeAttachedInCompactHeight,
+                selectedDetentIdentifier: selectedDetentIdentifier,
+                widthFollowsPreferredContentSizeWhenEdgeAttached: widthFollowsPreferredContentSizeWhenEdgeAttached,
+                isModalInPresentation: isModalInPresentation,
+                onDismiss: onDismiss,
+                contentView: contentView
+            )
+        )
+    }
+    
+    public func bottomSheet<T: Any, ContentView: View>(
+        item: Binding<T?>,
+        detents: [UISheetPresentationController.Detent] = [.medium(), .large()],
+        largestUndimmedDetentIdentifier: UISheetPresentationController.Detent.Identifier? = nil,
+        prefersGrabberVisible: Bool = false,
+        prefersScrollingExpandsWhenScrolledToEdge: Bool = true,
+        prefersEdgeAttachedInCompactHeight: Bool = false,
+        selectedDetentIdentifier: Binding<UISheetPresentationController.Detent.Identifier?> = Binding.constant(nil),
+        widthFollowsPreferredContentSizeWhenEdgeAttached: Bool = false,
+        isModalInPresentation: Bool = false,
+        onDismiss: (() -> Void)? = nil,
+        @ViewBuilder contentView: @escaping () -> ContentView
+    ) -> some View {
+        self.modifier(
+            BottomSheet(
+                item: item,
+                detents: detents,
+                largestUndimmedDetentIdentifier: largestUndimmedDetentIdentifier,
+                prefersGrabberVisible: prefersGrabberVisible,
+                prefersScrollingExpandsWhenScrolledToEdge: prefersScrollingExpandsWhenScrolledToEdge,
+                prefersEdgeAttachedInCompactHeight: prefersEdgeAttachedInCompactHeight,
+                selectedDetentIdentifier: selectedDetentIdentifier,
+                widthFollowsPreferredContentSizeWhenEdgeAttached: widthFollowsPreferredContentSizeWhenEdgeAttached,
+                isModalInPresentation: isModalInPresentation,
+                onDismiss: onDismiss,
+                contentView: contentView
+            )
+        )
+    }
+}
---
+++ Sources/EssentialUI/BottomSheet/BottomSheetViewController.swift
@@ -0,0 +1,109 @@
+//
+//  BottomSheetViewController.swift
+//  EssentialUI
+//
+//  Created by Gene Bogdanovich on 18.05.22.
+//
+
+import SwiftUI
+
+class BottomSheetViewController<Content: View>: UIViewController, UISheetPresentationControllerDelegate {
+    
+    // MARK: Properties
+    
+    @Binding private var isPresented: Bool
+    
+    private let detents: [UISheetPresentationController.Detent]
+    private let largestUndimmedDetentIdentifier: UISheetPresentationController.Detent.Identifier?
+    private let prefersGrabberVisible: Bool
+    private let prefersScrollingExpandsWhenScrolledToEdge: Bool
+    private let prefersEdgeAttachedInCompactHeight: Bool
+    @Binding private var selectedDetentIdentifier: UISheetPresentationController.Detent.Identifier?
+    private let widthFollowsPreferredContentSizeWhenEdgeAttached: Bool
+    
+    private let contentView: UIHostingController<Content>
+    
+    // MARK: Initialization
+    
+    init(
+        isPresented: Binding<Bool>,
+        detents: [UISheetPresentationController.Detent] = [.medium(), .large()],
+        largestUndimmedDetentIdentifier: UISheetPresentationController.Detent.Identifier? = nil,
+        prefersGrabberVisible: Bool = false,
+        prefersScrollingExpandsWhenScrolledToEdge: Bool = true,
+        prefersEdgeAttachedInCompactHeight: Bool = false,
+        selectedDetentIdentifier: Binding<UISheetPresentationController.Detent.Identifier?> = Binding.constant(nil),
+        widthFollowsPreferredContentSizeWhenEdgeAttached: Bool = false,
+        isModalInPresentation: Bool = false,
+        content: Content
+    ) {
+        _isPresented = isPresented
+        
+        self.detents = detents
+        self.largestUndimmedDetentIdentifier = largestUndimmedDetentIdentifier
+        self.prefersGrabberVisible = prefersGrabberVisible
+        self.prefersScrollingExpandsWhenScrolledToEdge = prefersScrollingExpandsWhenScrolledToEdge
+        self.prefersEdgeAttachedInCompactHeight = prefersEdgeAttachedInCompactHeight
+        self._selectedDetentIdentifier = selectedDetentIdentifier
+        self.widthFollowsPreferredContentSizeWhenEdgeAttached = widthFollowsPreferredContentSizeWhenEdgeAttached
+        
+        self.contentView = UIHostingController(rootView: content)
+        
+        super.init(nibName: nil, bundle: nil)
+        self.isModalInPresentation = isModalInPresentation
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addChild(contentView)
+        view.addSubview(contentView.view)
+        
+        contentView.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            contentView.view.topAnchor.constraint(equalTo: view.topAnchor),
+            contentView.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            contentView.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            contentView.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
+        
+        if let presentationController = presentationController as? UISheetPresentationController {
+            presentationController.detents = detents
+            presentationController.largestUndimmedDetentIdentifier = largestUndimmedDetentIdentifier
+            presentationController.prefersGrabberVisible = prefersGrabberVisible
+            presentationController.prefersScrollingExpandsWhenScrolledToEdge = prefersScrollingExpandsWhenScrolledToEdge
+            presentationController.prefersEdgeAttachedInCompactHeight = prefersEdgeAttachedInCompactHeight
+            presentationController.selectedDetentIdentifier = selectedDetentIdentifier
+            presentationController.widthFollowsPreferredContentSizeWhenEdgeAttached = widthFollowsPreferredContentSizeWhenEdgeAttached
+            presentationController.delegate = self
+        }
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        isPresented = false
+    }
+    
+    // MARK: Helpers
+    
+    func updateSelectedDetentIdentifier(_ selectedDetentIdentifier: UISheetPresentationController.Detent.Identifier?) {
+        self.sheetPresentationController?.animateChanges {
+            self.sheetPresentationController?.selectedDetentIdentifier = selectedDetentIdentifier
+        }
+    }
+    
+    // MARK: UISheetPresentationControllerDelegate
+    
+    func sheetPresentationControllerDidChangeSelectedDetentIdentifier(_ sheetPresentationController: UISheetPresentationController) {
+        self.selectedDetentIdentifier = sheetPresentationController.selectedDetentIdentifier
+    }
+}
