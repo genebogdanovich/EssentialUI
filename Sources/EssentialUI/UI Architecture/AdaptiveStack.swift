127ab4adb777c61274a687a09078b0582ed5948e
ProgressGuard <help@progressguard.com>
2022-05-22 18:01:00 +0300

    Fix adaptive stack.

--- Sources/EssentialUI/UI Architecture/AdaptiveStack.swift
+++ Sources/EssentialUI/UI Architecture/AdaptiveStack.swift
@@ -8,22 +8,22 @@
 import SwiftUI
 
 public struct AdaptiveStack<Content: View>: View {
-    @Environment(\.horizontalSizeClass) private var sizeClass
+    @Environment(\.dynamicTypeSize) private var dynamicTypeSize
     private let horizontalAlignment: HorizontalAlignment
     private let verticalAlignment: VerticalAlignment
     private let spacing: CGFloat?
     private let content: () -> Content
-
+    
     public init(horizontalAlignment: HorizontalAlignment = .center, verticalAlignment: VerticalAlignment = .center, spacing: CGFloat? = nil, @ViewBuilder content: @escaping () -> Content) {
         self.horizontalAlignment = horizontalAlignment
         self.verticalAlignment = verticalAlignment
         self.spacing = spacing
         self.content = content
     }
-
+    
     public var body: some View {
         Group {
-            if sizeClass == .compact {
+            if dynamicTypeSize.isAccessibilitySize {
                 VStack(alignment: horizontalAlignment, spacing: spacing, content: content)
             } else {
                 HStack(alignment: verticalAlignment, spacing: spacing, content: content)
