e81ebfccb2a65ff3fe8ad12c49eaf67ccc410cc3
ProgressGuard <help@progressguard.com>
2022-05-22 17:42:25 +0300

    Update access control.

--- Sources/EssentialUI/UI Architecture/AdaptiveStack.swift
+++ Sources/EssentialUI/UI Architecture/AdaptiveStack.swift
@@ -7,21 +7,21 @@
 
 import SwiftUI
 
-struct AdaptiveStack<Content: View>: View {
-    @Environment(\.horizontalSizeClass) var sizeClass
-    let horizontalAlignment: HorizontalAlignment
-    let verticalAlignment: VerticalAlignment
-    let spacing: CGFloat?
-    let content: () -> Content
+public struct AdaptiveStack<Content: View>: View {
+    @Environment(\.horizontalSizeClass) private var sizeClass
+    private let horizontalAlignment: HorizontalAlignment
+    private let verticalAlignment: VerticalAlignment
+    private let spacing: CGFloat?
+    private let content: () -> Content
 
-    init(horizontalAlignment: HorizontalAlignment = .center, verticalAlignment: VerticalAlignment = .center, spacing: CGFloat? = nil, @ViewBuilder content: @escaping () -> Content) {
+    public init(horizontalAlignment: HorizontalAlignment = .center, verticalAlignment: VerticalAlignment = .center, spacing: CGFloat? = nil, @ViewBuilder content: @escaping () -> Content) {
         self.horizontalAlignment = horizontalAlignment
         self.verticalAlignment = verticalAlignment
         self.spacing = spacing
         self.content = content
     }
 
-    var body: some View {
+    public var body: some View {
         Group {
             if sizeClass == .compact {
                 VStack(alignment: horizontalAlignment, spacing: spacing, content: content)
