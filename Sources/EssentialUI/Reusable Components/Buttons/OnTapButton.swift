37097fa7dc001008274f1f58465c6d8779c2402e
ProgressGuard <help@progressguard.com>
2022-05-23 15:40:43 +0300

    Add on tap button.

---
+++ Sources/EssentialUI/Reusable Components/Buttons/OnTapButton.swift
@@ -0,0 +1,44 @@
+//
+//  OnTapButton.swift
+//  EssentialUI
+//
+//  Created by Gene Bogdanovich on 23.05.22.
+//
+
+import SwiftUI
+
+public struct OnTapButton<Label: View>: View {
+    private let action: () -> Void
+    private let label: Label
+    
+    public init(
+        action: @escaping () -> Void,
+        @ViewBuilder label: () -> Label
+    ) {
+        self.action = action
+        self.label = label()
+    }
+    
+    public var body: some View {
+        label
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .contentShape(Rectangle())
+            .onTapGesture(perform: action)
+    }
+}
+
+struct OnTapButton_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            OnTapButton {
+            } label: {
+                Label("Add Image", systemImage: "photo")
+            }
+            OnTapButton {
+            } label: {
+                Text("Add URL")
+            }
+        }
+        .previewLayout(.sizeThatFits)
+    }
+}
