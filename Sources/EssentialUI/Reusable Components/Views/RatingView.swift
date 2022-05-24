17baf2602f12ba48ef704bb18a28db84a02027cd
ProgressGuard <help@progressguard.com>
2022-05-24 09:54:22 +0300

    Update RatingView.

--- Sources/EssentialUI/Reusable Components/Views/RatingView.swift
+++ Sources/EssentialUI/Reusable Components/Views/RatingView.swift
@@ -2,35 +2,32 @@
 //  RatingView.swift
 //  EssentialUI
 //
-//  Created by Gene Bogdanovich on 22.05.22.
+//  Created by Gene Bogdanovich on 24.05.22.
 //
 
 import SwiftUI
 
 public struct RatingView: View {
     @Binding private var rating: Int
-    
     private let label: String
-    private let maxRating: Int
-    
+    private let maximumRating: Int
     private let offImage: Image?
     private let onImage: Image
-    
     private let offColor: Color
     private let onColor: Color
     
     public init(
         rating: Binding<Int>,
         label: String = "",
-        maxRating: Int = 5,
+        maximumRating: Int = 5,
         offImage: Image? = nil,
         onImage: Image = Image(systemName: "star.fill"),
-        offColor: Color = .gray,
-        onColor: Color = .yellow
+        offColor: Color = Color.gray,
+        onColor: Color = Color.yellow
     ) {
         self._rating = rating
         self.label = label
-        self.maxRating = maxRating
+        self.maximumRating = maximumRating
         self.offImage = offImage
         self.onImage = onImage
         self.offColor = offColor
@@ -39,35 +36,45 @@ public struct RatingView: View {
     
     public var body: some View {
         HStack {
-            if !label.isEmpty {
+            if label.isEmpty == false {
                 Text(label)
             }
             
-            ForEach(1..<maxRating + 1, id: \.self) { number in
+            ForEach(1..<maximumRating + 1, id: \.self) { number in
                 image(for: number)
-                    .font(.callout)
                     .foregroundColor(number > rating ? offColor : onColor)
                     .onTapGesture {
-                        adjustRating(forNumber: number)
+                        adjustRating(for: number)
                     }
             }
         }
-    }
-    
-    private func adjustRating(forNumber number: Int) {
-        if rating == 1 && number == 1 {
-            rating = 0
-        } else {
-            rating = number
+        .accessibilityElement()
+        .accessibilityLabel("Rating")
+        .accessibilityValue("\(rating)")
+        .accessibilityAdjustableAction { direction in
+            switch direction {
+            case .increment:
+                if rating < maximumRating { rating += 1 }
+            case .decrement:
+                if rating > 1 { rating -= 1 }
+            default:
+                break
+            }
         }
     }
-    
-    private func image(for number: Int) -> Image {
+}
+
+extension RatingView {
+    func image(for number: Int) -> Image {
         if number > rating {
             return offImage ?? onImage
         } else {
             return onImage
         }
+    }
+    
+    func adjustRating(for number: Int) {
+        rating = number
     }
 }
 
