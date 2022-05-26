11dfa322b04df9988359fb1344ea7ae24c582d31
ProgressGuard <help@progressguard.com>
2022-05-26 08:19:43 +0300

    Update access control.

--- Sources/EssentialUI/UI Architecture/FlexibleView/FlexibleView.swift
+++ Sources/EssentialUI/UI Architecture/FlexibleView/FlexibleView.swift
@@ -9,14 +9,26 @@ import SwiftUI
 
 /// Facade of our view, its main responsibility is to get the available width
 /// and pass it down to the real implementation, `_FlexibleView`.
-struct FlexibleView<Data: Collection, Content: View>: View where Data.Element: Hashable {
+public struct FlexibleView<Data: Collection, Content: View>: View where Data.Element: Hashable {
     let data: Data
     let spacing: CGFloat
     let alignment: HorizontalAlignment
     let content: (Data.Element) -> Content
     @State private var availableWidth: CGFloat = 0
     
-    var body: some View {
+    public init(
+        data: Data,
+        spacing: CGFloat,
+        alignment: HorizontalAlignment,
+        content: @escaping (Data.Element) -> Content
+    ) {
+        self.data = data
+        self.spacing = spacing
+        self.alignment = alignment
+        self.content = content
+    }
+    
+    public var body: some View {
         ZStack(alignment: Alignment(horizontal: alignment, vertical: .center)) {
             Color.clear
                 .frame(height: 1)
