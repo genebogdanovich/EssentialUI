3f842e1fd40b90e3a55d52cf16773ea495bb72dd
ProgressGuard <help@progressguard.com>
2022-06-05 12:51:45 +0300

    Update text field.

--- Sources/EssentialUI/UI Architecture/UITextField.View.swift
+++ Sources/EssentialUI/UI Architecture/UITextField.View.swift
@@ -64,9 +64,21 @@ extension UITextField.View: UIViewRepresentable {
 
 extension UITextField.Delegate: UITextFieldDelegate {
     // UIKit -> StringUI
+    /*
     func textFieldDidEndEditing(_ textField: UITextField) {
         guard let text = textField.text else { return }
         self.text = text
+    }
+     */
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let text = textField.text else { return true }
+        let currentText = text
+        
+        let replacementText = (currentText as NSString).replacingCharacters(in: range, with: string)
+        self.text = replacementText
+        
+        return true
     }
 }
 
